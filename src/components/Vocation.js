7d399559ab40e38c1f159719918f2ae72f82218d
SheraliTukhtayev <sheralitukhtayev@gmail.com>
2022-10-06 17:42:08 +0500

    some changes

--- src/components/Vocation.js
+++ src/components/Vocation.js
@@ -1,80 +1,121 @@
-import React from 'react'
-import style from "styled-components"
+import React from "react";
+import style from "styled-components";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function Vocation() {
   return (
     <>
-        <Container>
-            <Heading>
-                <h1>ВАКАНСИИ</h1>
-            </Heading>
-            <Text>
-                <h1>Наши ценности:</h1>
-                <p>Мы стремимся поддерживать культуру взаимопомощи, главные принципы которой уважение и доверие в коллективе, вдохновение от руководителей на дальнейшие свершения.Мы верим в то, что улучшим наши показатели благодаря мотивации, обучению и развитию наших сотрудников. Компания поддерживает карьерный рост и предоставляет широкие возможности для развития.</p>
-                <p>Работать в Lift Universal – значит быть в сердце компании, быть тем, кто каждый день делает жизнь людей проще. Мы гордимся, что можем поделиться опытом и предложить вам возможности, которые помогут вам достичь карьерных и личностных целей.</p>
+      <Container>
+        <Heading>
+          <h1>ВАКАНСИИ</h1>
+        </Heading>
+        <Text>
+          <h1>Наши ценности:</h1>
+          <p>
+            Мы стремимся поддерживать культуру взаимопомощи, главные принципы
+            которой уважение и доверие в коллективе, вдохновение от
+            руководителей на дальнейшие свершения.Мы верим в то, что улучшим
+            наши показатели благодаря мотивации, обучению и развитию наших
+            сотрудников. Компания поддерживает карьерный рост и предоставляет
+            широкие возможности для развития.
+          </p>
+          <p>
+            Работать в Lift Universal – значит быть в сердце компании, быть тем,
+            кто каждый день делает жизнь людей проще. Мы гордимся, что можем
+            поделиться опытом и предложить вам возможности, которые помогут вам
+            достичь карьерных и личностных целей.
+          </p>
 
-                <h1>Кто нам нужен?</h1>
+          <h1>Кто нам нужен?</h1>
 
-                <AboutTextCont>
-                    <AboutText>
-                        <h3>ОПЫТНЫЕ СПЕЦИАЛИСТЫ И РУКОВОДИТЕЛИ</h3>
-                        <p>Мы предлагаем специалистам из разных областей возможность работать в сфере высокотехнологичного оборудования. В дополнении к этому, мы работаем с комплексными сервисами и инструментами, обеспечивающими нашу производительность 24/7. Больше половины наших продаж являются результатом предложенного сервиса.</p>
-                    </AboutText>
-                    <AboutText>
-                        <h3>ПРОФЕССИОНАЛЫ В СФЕРЕ БИЗНЕСА</h3>
-                        <p>Для перспективных лидеров мы предлагаем возможности по поддержке личностного роста и профессионального развития. Наш подход к менеджменту подразумевает регулярные обсуждения показателей продуктивности.</p>
-                    </AboutText>
-                    <AboutText>
-                        <h3>ТЕХНИЧЕСКИЕ СПЕЦИАЛИСТЫ</h3>
-                        <p>Монтажники и наладчики – это эксперты, которые заботятся о безопасности и качественной установке такого оборудования, как лифты и эскалаторы.</p>
-                        <p>Электромеханики по лифтам и машинисты эскалаторов осуществляют сервисное обслуживание оборудования и ежедневно встречаются с клиентами, показывая истинно сервисный подход.</p>
-                    </AboutText>
-                </AboutTextCont>
+          <AboutTextCont>
+            <AboutText>
+              <h3>ОПЫТНЫЕ СПЕЦИАЛИСТЫ И РУКОВОДИТЕЛИ</h3>
+              <p>
+                Мы предлагаем специалистам из разных областей возможность
+                работать в сфере высокотехнологичного оборудования. В дополнении
+                к этому, мы работаем с комплексными сервисами и инструментами,
+                обеспечивающими нашу производительность 24/7. Больше половины
+                наших продаж являются результатом предложенного сервиса.
+              </p>
+            </AboutText>
+            <AboutText>
+              <h3>ПРОФЕССИОНАЛЫ В СФЕРЕ БИЗНЕСА</h3>
+              <p>
+                Для перспективных лидеров мы предлагаем возможности по поддержке
+                личностного роста и профессионального развития. Наш подход к
+                менеджменту подразумевает регулярные обсуждения показателей
+                продуктивности.
+              </p>
+            </AboutText>
+            <AboutText>
+              <h3>ТЕХНИЧЕСКИЕ СПЕЦИАЛИСТЫ</h3>
+              <p>
+                Монтажники и наладчики – это эксперты, которые заботятся о
+                безопасности и качественной установке такого оборудования, как
+                лифты и эскалаторы.
+              </p>
+              <p>
+                Электромеханики по лифтам и машинисты эскалаторов осуществляют
+                сервисное обслуживание оборудования и ежедневно встречаются с
+                клиентами, показывая истинно сервисный подход.
+              </p>
+            </AboutText>
+          </AboutTextCont>
 
-                <button>Отправить резюме</button>
+          <button>Отправить резюме</button>
 
-                <h4>Отправьте свое резюме на интересующую вакансию по электронному адресу info@lift-universal.uz , указав в теме письма название вакансии.</h4>
-                <h2>ВАКАНСИЯ</h2>
-                <li>Электромеханик по лифтам</li>
-                <li>Менеджер по модернизации и замене оборудования</li>
-                <li>Монтажник электрических лифтов</li>
-            </Text>
-            <Resume>
-                <h1>Отправить резюме</h1>
-                <ResCont>
-                <form action="https://formsubmit.co/sheralitukhtayev@gmail.com" method="post">
-                    <Inputs>
-                        <h6><span>*</span> Вакансия</h6>
-                        <input placeholder="Не вибрано"/>
-                    </Inputs>
-                    <Inputs>
-                        <h6><span>*</span> Имя</h6>
-                        <input type="text" />
-                    </Inputs>
-                    <Inputs>
-                        <h6><span>*</span> Телефон</h6>
-                        <input type="number" />
-                    </Inputs>
-                    <Inputs>
-                        <h6><span>*</span> E-mail</h6>
-                        <input type="email" />
-                    </Inputs>
-                    <h5>Загрузка файла</h5>
-                    <div className="Btn">
-                        <button>Выбрать файл(ы)</button>
-                        <p>не болуу:1</p>
-                    </div>
-                    <Inputs>
-                        <h6>Комментарий</h6>
-                        <textarea className="Comment" name="message" id=""></textarea>
-                    </Inputs>
-                 <button type="submit">Send</button>
-                </form>
-                </ResCont>
-            </Resume>
-        </Container>
+          <h4>
+            Отправьте свое резюме на интересующую вакансию по электронному
+            адресу info@lift-universal.uz , указав в теме письма название
+            вакансии.
+          </h4>
+          <h2>ВАКАНСИЯ</h2>
+          <li>Электромеханик по лифтам</li>
+          <li>Менеджер по модернизации и замене оборудования</li>
+          <li>Монтажник электрических лифтов</li>
+        </Text>
+        <Resume>
+          <h1>Отправить резюме</h1>
+          <ResCont>
+            <form
+              action="https://formsubmit.co/sheralitukhtayev@gmail.com"
+              method="POST"
+            >
+              <Inputs>
+                <h6>
+                  <span>*</span> Вакансия
+                </h6>
+                <input placeholder="Не вибрано" required />
+              </Inputs>
+              <Inputs>
+                <h6>
+                  <span>*</span> Имя
+                </h6>
+                <input type="text" required />
+              </Inputs>
+              <Inputs>
+                <h6>
+                  <span>*</span> Телефон
+                </h6>
+                <input type="number" required />
+              </Inputs>
+              <Inputs>
+                <h6>
+                  <span>*</span> E-mail
+                </h6>
+                <input type="email" required />
+              </Inputs>
+              <h5>Загрузка файла</h5>
+              <button className="btn" type="submit">
+                Send
+              </button>
+            </form>
+          </ResCont>
+        </Resume>
+      </Container>
     </>
-  )
+  );
 }
 
 const Container = style.div`
@@ -82,7 +123,7 @@ const Container = style.div`
     margin: 0 auto;
     padding: 100px 0;
 
-`
+`;
 
 const Heading = style.div`
     h1 {
@@ -91,7 +132,7 @@ const Heading = style.div`
         margin: 40px 0;
     }
 
-`
+`;
 
 const Text = style.div`
     width: 65vw;
@@ -102,7 +143,7 @@ const Text = style.div`
         font-size: 40px;
         margin: 10px 0;
     }
-     
+
     p{
         font-size: 20px;
         line-height: 30px;
@@ -129,7 +170,7 @@ const Text = style.div`
         color: rgba(0, 0, 0, 0.7);
     }
     h2 {
-        font-size: 30px; 
+        font-size: 30px;
         font-weight: 700;
         color: rgb(45, 68, 171);
         margin: 20px 0;
@@ -144,14 +185,14 @@ const Text = style.div`
         color: rgba(0, 0, 0, 0.8);
     }
 
-`
+`;
 
 const AboutTextCont = style.div`
     display: flex;
     flex-direction: column;
 
 
-`
+`;
 
 const AboutText = style.div`
     display: flex;
@@ -166,19 +207,19 @@ const AboutText = style.div`
         font-weight: 600;
         color: rgba(0, 0, 0, 0.8);
     }
-     
+
     p {
         margin: 0;
-        margin-bottom: 10px; 
+        margin-bottom: 10px;
         font-size: 12px;
         line-height: 17px;
     }
 
-`
+`;
 
 const Resume = style.div`
     width: 80vw;
-    background: rgb(45, 68, 171); 
+    background: rgb(45, 68, 171);
     text-align: center;
     padding: 70px 0;
     color: white;
@@ -187,7 +228,7 @@ const Resume = style.div`
         font-size: 40px;
     }
 
-`
+`;
 
 const ResCont = style.div`
     width: 50%;
@@ -207,7 +248,7 @@ const ResCont = style.div`
         align-items: center;
         margin-left: 55px;
         margin-bottom: 40px;
-         
+
         button {
             font-size: 13px;
         }
@@ -235,7 +276,7 @@ const ResCont = style.div`
         text-align: left;
     }
 
-`
+`;
 
 const Inputs = style.div`
     margin: 20px 0;
@@ -271,8 +312,8 @@ const Inputs = style.div`
         }
     }
 
-    
 
-`
 
-export default Vocation
+`;
+
+export default Vocation;
